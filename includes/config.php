4466209d163874bbff691547ad2cb99e4ad8b2f2
escripcari <84279941+escripcari@users.noreply.github.com>
2022-07-27 13:44:55 +0300

    rollback config

--- includes/config.php
+++ includes/config.php
@@ -172,10 +172,10 @@ $config = array(
 		array(
 			'settings' =>
 				array(
-					'host'       => 'localhost',
-					'login'      => 'root',
-					'pass'       => '',
-					'database'   => 'laser',
+                    'host'       => '83.69.230.31',
+                    'login'      => 'vh88807_new',
+                    'pass'       => 'wUKooL9G',
+                    'database'   => 'vh88807_release',
 					'error_mode' => (TEST_MODE) ? 2 : 1  // 1 - short error ("MySQL Error"); 2 - full error
 				),
 			'tables' =>
